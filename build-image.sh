7cb2c985e14a95134bd4e68aba2c24afb8c6f0ea
Frank Hamand <frankhamand@gmail.com>
2017-02-18 18:41:27 +0000

    Add 0.6.0 build

--- build-image.sh
+++ build-image.sh
@@ -4,4 +4,5 @@ docker build -t cabot-build -f Dockerfile.build .
 rm -rf build/*.whl
 docker run --rm -v `pwd`/build:/build cabot-build "cp /code/dist/*.whl /build/"
 CABOT_VERSION=`docker run --rm cabot-build 'git describe --tags'`
+cp build/*.whl $CABOT_VERSION/
 docker build --squash -t cabotapp/cabot -t cabotapp/cabot:$CABOT_VERSION -f $CABOT_VERSION/Dockerfile $CABOT_VERSION/
