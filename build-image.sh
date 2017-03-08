26f7cc7512ca4c455b1f4e65c5f974252b99986d
Frank Hamand <frankhamand@gmail.com>
2017-03-08 13:05:29 +0000

    0.9.0

--- build-image.sh
+++ build-image.sh
@@ -7,3 +7,5 @@ CABOT_VERSION=`docker run --rm cabot-build 'git describe --tags'`
 cp build/*.whl $CABOT_VERSION/
 docker build --squash -t cabotapp/cabot -t cabotapp/cabot:$CABOT_VERSION -f $CABOT_VERSION/Dockerfile $CABOT_VERSION/
 echo "Successfully built image cabotapp/cabot:$CABOT_VERSION"
+docker tag cabotapp/cabot:$CABOT_VERSION cabotapp/cabot:latest
+echo "Tagged image as cabotapp/cabot:latest"
