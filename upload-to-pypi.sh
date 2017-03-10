7f6d33821c2002b0073cff3512cd9165d5ab6b9e
Frank Hamand <frankhamand@gmail.com>
2017-03-10 09:59:31 +0000

    Add prompt to upload to pypi

--- upload-to-pypi.sh
+++ upload-to-pypi.sh
@@ -4,4 +4,12 @@ set -a
 source .env
 docker build -t cabot-build:$CABOT_VERSION --build-arg CABOT_VERSION=$CABOT_VERSION -f Dockerfile.build .
 
+while true; do
+    read -p "Are you sure you want to upload cabot version $CABOT_VERSION? (y/N) " yn
+    case $yn in
+        [Yy] ) break;;
+        * ) exit;;
+    esac
+done
+
 docker run --rm -v ~/.pypirc:/root/.pypirc cabot-build:$CABOT_VERSION "twine upload dist/*.whl dist/*.tar.gz"
