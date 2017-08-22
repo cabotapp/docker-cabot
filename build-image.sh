abe7d870da805620bc540278abfdbd71b7497988
Frank Hamand <frankhamand@gmail.com>
2017-08-22 11:08:23 +0100

    Cleanup build-image.sh a bit

--- build-image.sh
+++ build-image.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -e
 set -a
 source .env
-docker build --squash -t cabotapp/cabot -t cabotapp/cabot:$CABOT_VERSION --build-arg CABOT_VERSION=$CABOT_VERSION -f Dockerfile .
-echo "Successfully built image cabotapp/cabot:$CABOT_VERSION"
-docker tag cabotapp/cabot:$CABOT_VERSION cabotapp/cabot:latest
-echo "Tagged image as cabotapp/cabot:latest"
+docker build --squash -t cabotapp/cabot -t cabotapp/cabot:$CABOT_VERSION -t cabotapp/cabot:latest --build-arg CABOT_VERSION=$CABOT_VERSION -f Dockerfile .
